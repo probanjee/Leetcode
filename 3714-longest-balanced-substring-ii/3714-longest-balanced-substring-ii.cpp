239a1e4a1471e80e1f9c675b72e3381ab536d204
Prosun Banerjee <prosunbanerjee8@gmail.com>
2026-07-11 12:20:46 +0530

    Time: 254 ms (99.17%), Space: 143.8 MB (96.4%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
@@ -1,64 +1,80 @@
 class Solution {
 public:
     int longestBalanced(string s) {
-        int x = calc1(s);
-        int y = max({calc2(s, 'a', 'b'),
-                     calc2(s, 'b', 'c'),
-                     calc2(s, 'a', 'c')});
-        int z = calc3(s);
-        return max({x, y, z});
-    }
+        int n = (int)s.size();
+        int ans = 0;
 
-private:
-    int calc1(const string& s) {
-        int res = 0;
-        for (int i = 0; i < s.size();) {
-            int j = i;
-            while (j < s.size() && s[j] == s[i]) j++;
-            res = max(res, j - i);
-            i = j;
+        for (int i = 0; i < n; ) {
+            int start = i;
+            i++;
+            while (i < n && s[i] == s[i - 1]) i++;
+            ans = max(ans, i - start);
         }
-        return res;
-    }
 
-    int calc2(const string& s, char a, char b) {
-        int res = 0, n = s.size(), i = 0;
-        while (i < n) {
-            while (i < n && s[i] != a && s[i] != b) i++;
-            unordered_map<int,int> mp;
-            mp[0] = i - 1;
-            int diff = 0;
-            while (i < n && (s[i] == a || s[i] == b)) {
-                diff += (s[i] == a ? 1 : -1);
-                if (mp.count(diff))
-                    res = max(res, i - mp[diff]);
-                else
-                    mp[diff] = i;
-                i++;
+        auto twoChars = [&](char x, char y) {
+            int i = 0;
+            vector<int> first(2 * n + 1, INT_MAX);
+            vector<int> touched;
+            touched.reserve(2 * n + 1);
+
+            while (i < n) {
+                int segStart = i;
+                first[n] = segStart - 1;
+                touched.push_back(n);
+
+                int d = 0;
+                while (i < n && (s[i] == x || s[i] == y)) {
+                    d += (s[i] == x) ? 1 : -1;
+                    int idx = d + n;
+                    if (first[idx] != INT_MAX) {
+                        ans = max(ans, i - first[idx]);
+                    } else {
+                        first[idx] = i;
+                        touched.push_back(idx);
+                    }
+                    i++;
+                }
+
+                for (int idx : touched) 
+                    first[idx] = INT_MAX;
+                touched.clear();
+
+                i++; 
             }
-        }
-        return res;
-    }
+        };
 
-    long long key(int x, int y) {
-        return ((long long)(x + 100000) << 20) | (y + 100000);
-    }
+        twoChars('a','b');
+        twoChars('a','c');
+        twoChars('b','c');
 
-    int calc3(const string& s) {
-        unordered_map<long long,int> mp;
-        mp[key(0,0)] = -1;
-        int cnt[3] = {0};
-        int res = 0;
-        for (int i = 0; i < s.size(); i++) {
-            cnt[s[i] - 'a']++;
-            int x = cnt[0] - cnt[1];
-            int y = cnt[1] - cnt[2];
-            long long k = key(x,y);
-            if (mp.count(k))
-                res = max(res, i - mp[k]);
-            else
-                mp[k] = i;
+        unordered_map<long long, int> pos;
+        pos.reserve(n * 2 + 1);
+
+        auto key = [&](int d1, int d2) -> long long {
+            return ( (long long)d1 << 32 ) ^ (unsigned int)d2;
+        };
+
+        int ca = 0, cb = 0, cc = 0;
+        pos[key(0,0)] = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a') 
+                ca++;
+            else if (s[i] == 'b') 
+                cb++;
+            else if (s[i] == 'c') 
+                cc++;
+
+            int d1 = ca - cb;
+            int d2 = cb - cc;
+            long long k = key(d1, d2);
+
+            auto it = pos.find(k);
+            if (it != pos.end()) 
+                ans = max(ans, i - it->second);
+            else pos[k] = i;
         }
-        return res;
+
+        return ans;
     }
 };
