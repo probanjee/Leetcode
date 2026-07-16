25ed8c5fe5dba41ae245285dff1c2ba0b0f49e65
Prosun Banerjee <prosunbanerjee8@gmail.com>
2026-07-17 02:39:04 +0530

    Time: 5 ms (19.74%), Space: 8.6 MB (37.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        if(x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
+        }
+
+        int halfRev = 0;
+
+        while(halfRev < x){
+
+            halfRev = halfRev * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return halfRev == x || halfRev / 10 == x;
+    }
+};
