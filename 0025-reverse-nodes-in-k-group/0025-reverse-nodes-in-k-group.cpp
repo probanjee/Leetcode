66b56076d9387dc963af68790ae1f1b73f9d0d0a
Prosun Banerjee <prosunbanerjee8@gmail.com>
2026-07-18 13:06:25 +0530

    Time: 0 ms (100%), Space: 16.4 MB (71.11%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || !head->next || k <= 1)
+            return head;
+
+        // Dummy node to handle edge cases
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prevGroupEnd = &dummy;
+
+        while (true) {
+            // Check if there's a complete group of size k
+            ListNode* kth = prevGroupEnd;
+            for (int i = 0; i < k && kth; ++i)
+                kth = kth->next;
+            if (!kth)
+                break; // Not enough nodes for a complete group
+
+            ListNode* groupStart = prevGroupEnd->next;
+            ListNode* nextGroupStart = kth->next;
+
+            // Reverse the group
+            ListNode* prev = nextGroupStart;
+            ListNode* curr = groupStart;
+            while (curr != nextGroupStart) {
+                ListNode* tmp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = tmp;
+            }
+
+            // Connect the reversed group
+            prevGroupEnd->next = kth;
+            prevGroupEnd = groupStart;
+        }
+
+        return dummy.next;
+    }
+};
