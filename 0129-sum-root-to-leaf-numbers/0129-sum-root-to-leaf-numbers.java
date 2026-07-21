436b2c9b77a887ad30e0457cdfb7d799010f23a8
Prosun Banerjee <prosunbanerjee8@gmail.com>
2026-07-21 23:17:49 +0530

    Time: 0 ms (100%), Space: 42.8 MB (67.67%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);        
+    }
+
+    private int dfs(TreeNode node, int num) {
+        if (node == null) {
+            return 0;
+        }
+        
+        num = num * 10 + node.val;
+        
+        if (node.left == null && node.right == null) {
+            return num;
+        }
+        
+        return dfs(node.left, num) + dfs(node.right, num);
+    }    
+}
