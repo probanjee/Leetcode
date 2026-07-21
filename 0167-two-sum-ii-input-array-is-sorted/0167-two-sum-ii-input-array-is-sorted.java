c3a8b08604ac1063bb72d9cb64de973146862d36
Prosun Banerjee <prosunbanerjee8@gmail.com>
2026-07-22 04:53:02 +0530

    Time: 2 ms (95.57%), Space: 48.5 MB (42.2%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        //"1-indexed" just means: when they describe the answer as numbers[index1], they're counting the first element as position 1, not position 0.
+        int i=0;
+        int j=numbers.length-1; //1 indexxed array n both diff ele
+        int res[]= new int[2];
+
+        while(i<j)
+        {
+            int sum= numbers[i]+numbers[j];
+
+            if(sum== target)
+            {
+                res[0]=i+1;
+                res[1]=j+1;
+                break;
+            }
+            else if(sum<target)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return res;
+    }
+}
