a57ada4aa136050463b731363f0965344ab6748b
Prosun Banerjee <prosunbanerjee8@gmail.com>
2026-08-05 00:50:19 +0530

    Time: 28 ms (15.37%), Space: 10.7 MB (30.05%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.go
@@ -0,0 +1,10 @@
+func findDuplicate(nums []int) int {
+    seen := make(map[int]bool)
+    for _, num := range nums {
+        if seen[num] {
+            return num
+        }
+        seen[num] = true
+    }
+    return -1  
+}
