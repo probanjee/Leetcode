3f3325cb278e9503c2d6e775bbd752a842112d09
Prosun Banerjee <prosunbanerjee8@gmail.com>
2026-08-05 00:43:45 +0530

    Time: 3 ms (16.66%), Space: 8 MB (42.15%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left=1,mid;
+        while(left<=n){
+            mid=left+(n-left)/2;
+            if(isBadVersion(mid)){
+                n=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+};
